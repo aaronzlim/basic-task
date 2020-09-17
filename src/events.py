4db83dd3a7b68b4649364148fee0d80d9fe1a553
Aaron Lim <aaronzlim@gmail.com>
2020-09-17 08:20:37 -0400

    Refactored handle_event function for clearer sql.

--- src/events.py
+++ src/events.py
@@ -8,34 +8,48 @@ from src import database as db
 from src.layout import ID_INDEX, TASK_INDEX, STATUS_INDEX, DUE_INDEX, LABEL_INDEX
 
 def handle_event(window: Window, event: str, values: dict) -> str:
-    err_str = ''
     table = window['task-table'].get()
     selected_taskids = ','.join((str(table[row][ID_INDEX]) for row in values['task-table']))
     with db.connection() as conn:
-        c = conn.cursor()
-        if event == 'add-button':
-            new_task = values['task-inputtext']
-            if new_task:
-                c.execute(f"""
-                    INSERT INTO tasks (task, status)
-                    VALUES ('{new_task}', 'Incomplete')
-                    """)
+        try:
+            c = conn.cursor()
+            if event == 'add-button':
+                new_task = values['task-inputtext']
+                if new_task:
+                    c.execute(f"""
+                        INSERT INTO tasks (task, status)
+                        VALUES ('{new_task}', 'Incomplete')
+                        """)
 
-        elif event == 'delete-button' and selected_taskids:
-            try:
+            elif event == 'delete-button' and selected_taskids:
                 c.execute(f"DELETE FROM tasks WHERE taskid IN ({selected_taskids})")
-            except Error as e:
-                err_str += e
 
-        elif event =='status-button' and selected_taskids:
-            for row in values['task-table']:
-                new_status = 'Complete' if table[row][STATUS_INDEX] == 'Incomplete' else 'Incomplete'
-                task_id = table[row][ID_INDEX]
-                c.execute(f"""UPDATE tasks SET status='{new_status}' WHERE taskid={task_id}""")
+            elif event =='status-button' and selected_taskids:
+                to_complete = []
+                to_incomplete = []
+                for row in values['task-table']:
+                    if table[row][STATUS_INDEX] == 'Incomplete':
+                        to_complete.append(str(table[row][ID_INDEX]))
+                    else:
+                        to_incomplete.append(str(table[row][ID_INDEX]))
 
-        elif event == 'edit-button' and selected_taskids:
-            pass
+                if to_complete:
+                    task_ids = ','.join(to_complete)
+                    c.execute(f"""UPDATE tasks SET status='Complete' WHERE taskid IN ({task_ids})""")
 
-        if not err_str:
+                if to_incomplete:
+                    task_ids = ','.join(to_incomplete)
+                    c.execute(f"""UPDATE tasks SET status='Incomplete' WHERE taskid IN ({task_ids})""")
+
+            elif event == 'edit-button' and selected_taskids:
+                pass
+
             conn.commit()
+
+        except Error as e:
+            err_str = str(e)
+
+        else:
+            err_str = ''
+
     return err_str
